1108797cfeaaf85335cb9054c399909c220b9efa
NilSousa16 <nil.sousa16@gmail.com>
2021-01-18 12:06:20 -0300

    Change in the ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,10 @@
+console.log('process.env.DATABASE_URL: >>> ', process.env.DATABASE_URL);
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "entities": [
-    "./src/modules/**/infra/typeorm/entities/*.ts"
-    // "./dist/modules/**/infra/typeorm/entities/*.js"
+    // "./src/modules/**/infra/typeorm/entities/*.ts"
+    "./dist/modules/**/infra/typeorm/entities/*.js"
   ],
   "migrations": [
     // "./src/shared/infra/typeorm/migrations/*.ts"
