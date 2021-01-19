e18c64c05deab513788f30ce45ac3e58dd119c52
NilSousa16 <nil.sousa16@gmail.com>
2021-01-19 10:23:50 -0300

    Change ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,9 @@ console.log('process.env.DATABASE_URL: >>> ', process.env.DATABASE_URL);
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "ssl": {
+    rejectUnauthorized: false
+  },
   "entities": [
     // "./src/modules/**/infra/typeorm/entities/*.ts"
     "./dist/modules/**/infra/typeorm/entities/*.js"
